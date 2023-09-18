d6a34270f74dd43cc97f58276008de1d8cce44e4
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-09-18 00:17:17 -0400

    full CRUD operational, build complete | Next is deployment and changing the URL we fetch to the deployed heroku backend link connected to our database.

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Index from '../Pages/Index';
 import Show from '../Pages/Show';
 
@@ -15,9 +15,9 @@ function Main(props) {
       const data = await response.json();
       setPeople(data);
     } catch (error) {
-      // TODO: Add a task we wish to perform in the event of an error
+      // TODO: Add a task we'd like to perform in the event of an error
     }
-  }
+  };
 
   const createPeople = async (person) => {
     try {
@@ -30,9 +30,36 @@ function Main(props) {
       });
       getPeople();
     } catch (error) {
-      // TODO: Add a task we wish to perform in the event of an error
+      // TODO: Add a task we'd like to perform in the event of an error
     }
-  }
+  };
+
+
+// ... more code above
+
+// Inside of Main.js
+
+const updatePeople = async (person, id) => {
+    await fetch(URL + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+      body: JSON.stringify(person),
+    });
+    // update list of people
+    getPeople();
+  };
+
+// ... more code below
+
+
+const deletePeople = async (id) => {
+    await fetch(URL + id, {
+      method: 'DELETE',
+    });
+    getPeople();
+  };
 
   useEffect(() => {
     getPeople();
@@ -43,9 +70,23 @@ function Main(props) {
       <Routes>
         <Route 
           path="/" 
-          element={<Index people={people} createPeople={createPeople} />} 
+          element={
+            <Index 
+              people={people} 
+              createPeople={createPeople} 
+            />
+          }
         />
-        <Route path="/people/:id" element={<Show />} />
+        <Route
+          path="/people/:id"
+          element={
+            <Show
+              people={people}
+              updatePeople={updatePeople}
+              deletePeople={deletePeople}
+            />
+          }
+        />
       </Routes>
     </main>
   );
--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,9 +1,94 @@
-import React from 'react'
 
-function Show() {
+// importing the useNavigate hook
+import { useNavigate, useParams } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+
+function Show(props) {
+  // set up nav function with the useNavigate hook
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const people = props.people;
+  const person = people ? people.find((p) => p._id === id) : null;
+
+  const [editForm, setEditForm] = useState({
+    name: "",
+    title: "",
+    image: ""
+  });
+
+  // handleChange function for form
+  const handleChange = (event) => {
+    setEditForm(prevState => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }))
+  };
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    props.updatePeople(editForm);
+  };
+
+  const handleDelete = () => {
+    props.deletePeople(person._id);
+    navigate('/');
+  };
+
+  const loaded = () => {
+    return (
+      <>
+        <h1>{person.name}</h1>
+        <h2>{person.title}</h2>
+        <img 
+          className="avatar-image" 
+          src={person.image} 
+          alt={person.name} 
+        />
+        <button id="delete" onClick={handleDelete}>
+          DELETE
+        </button>
+      </>
+    );
+  };
+  const loading = () => {
+    return <h1>Loading ...</h1>;
+  };
+
+  useEffect(() => {
+    if(person) { 
+      setEditForm(person);
+    }
+  }, [person]);
+
   return (
-    <div>Show</div>
-  )
+    <div className="person">
+      { person ? loaded() : loading() }
+      <form onSubmit={handleUpdate}>
+        <input
+          type="text"
+          value={editForm.name}
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.image}
+          name="image"
+          placeholder="image URL"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={editForm.title}
+          name="title"
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Update Person" />
+      </form>
+    </div>
+  );
 }
 
-export default Show
+export default Show;
